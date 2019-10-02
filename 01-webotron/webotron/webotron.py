302a08330f40ad932ff147ced33841402fcbcb44
Wei_Mu <wei.mu@github.com>
2019-10-01 22:30:33 -0400

    Sync directory tree to s3

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -1,6 +1,7 @@
 import boto3
-from botocore.exception import ClientError
-import click
+from botocore.exceptions import ClientError
+import click, mimetypes
+from pathlib import Path
 
 session = boto3.Session(profile_name = 'pythonAutomation')
 s3 = session.resource('s3')
@@ -71,6 +72,35 @@ def setup_bucket(bucket):
     })
 
     return
+
+
+def upload_file(s3_bucket, path, key):
+    content_type = mimetypes.guess_type(key)[0] or 'text/plain'
+
+    s3_bucket.upload_file(
+    path,
+    key,
+    ExtraArgs = {
+        'ContentType' : content_type
+        }
+    )
+
+
+@cli.command('sync')
+@click.argument('pathname', type = click.Path(exists = True))
+@click.argument('bucket')
+def sync(pathname, bucket):
+    "Sync contents of PATHNAME to BUCKET"
+
+    root = Path(pathname).expanduser().resolve()
+    s3_bucket = s3.Bucket(bucket)
+
+    def handle_directory(target):
+        for p in target.iterdir():
+            if p.is_dir(): handle_directory(p)
+            if p.is_file(): upload_file(s3_bucket, str(p), str(p.relative_to(root)))
+
+    handle_directory(root)
 
 
 if __name__ == '__main__':
